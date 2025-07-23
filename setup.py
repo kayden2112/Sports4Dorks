7ab99a7ba913ecfd9b31a7c8683a336362fd414c
kayden2112 <kayden.bloodworth@gmail.com>
2025-07-22 21:35:01 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A sports betting algorithm for MLB Betting using python.',
+    author='Kayden Bloodworth',
+    license='MIT',
+)
